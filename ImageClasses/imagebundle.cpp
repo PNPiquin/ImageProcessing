950d7985e82b0f1c84c7a9398c388e665085c39b
Unknown <pierre-nicolas.piquin@mines-paristech.fr>
2018-07-03 21:13:51 +0200

    Update processing methods to handle img vector

--- ImageClasses/imagebundle.cpp
+++ ImageClasses/imagebundle.cpp
@@ -61,116 +61,184 @@ void ImageBundle::ProcessEdgeDetection(std::string img_name, std::string output_
         out_img_vector.push_back(edge_img);
     }
     Insert(output_name, out_img_vector);
-
-    //std::shared_ptr<ImageHolder> img = find_image(img_name);
-    //std::shared_ptr<ImageHolder> edge_img = img->ProcessEdgeDetection(output_name, filter_size, use_gaussian_blur, gaussian_filter_size, progress_bar);
-    //Insert(output_name, edge_img);
 }
 
 void ImageBundle::ProcessBothSobel(std::string img_name, std::string output_name, bool use_gaussian_blur, int gaussian_filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> edge_img = img->ProcessBothSobel(output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
-    Insert(output_name, edge_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> edge_img = img->ProcessBothSobel(img->GetImageName() + output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
+        out_img_vector.push_back(edge_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessVerticalSobel(std::string img_name, std::string output_name, bool use_gaussian_blur, int gaussian_filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> edge_img = img->ProcessVerticalSobel(output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
-    Insert(output_name, edge_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> edge_img = img->ProcessVerticalSobel(img->GetImageName() + output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
+        out_img_vector.push_back(edge_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessHorizontalSobel(std::string img_name, std::string output_name, bool use_gaussian_blur, int gaussian_filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> edge_img = img->ProcessHorizontalSobel(output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
-    Insert(output_name, edge_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> edge_img = img->ProcessHorizontalSobel(img->GetImageName() + output_name, use_gaussian_blur, gaussian_filter_size, progress_bar);
+        out_img_vector.push_back(edge_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessGaussianBlur(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> edge_img = img->ProcessGaussianBlur(output_name, filter_size, progress_bar);
-    Insert(output_name, edge_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> edge_img = img->ProcessGaussianBlur(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(edge_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessHistogramNormalization(std::string img_name, std::string output_name, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> histogram_img = img->ProcessHistogramNormalization(output_name, progress_bar);
-    Insert(output_name, histogram_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> histogram_img = img->ProcessHistogramNormalization(img->GetImageName() + output_name, progress_bar);
+        out_img_vector.push_back(histogram_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessPowerLawTransformation(std::string img_name, std::string output_name, double gamma, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> pow_img = img->ProcessPowerLawTransformation(output_name, gamma, progress_bar);
-    Insert(output_name, pow_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> pow_img = img->ProcessPowerLawTransformation(img->GetImageName() + output_name, gamma, progress_bar);
+        out_img_vector.push_back(pow_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessLogLawTransformation(std::string img_name, std::string output_name, double c, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> log_img = img->ProcessPowerLawTransformation(output_name, c, progress_bar);
-    Insert(output_name, log_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> log_img = img->ProcessPowerLawTransformation(img->GetImageName() + output_name, c, progress_bar);
+        out_img_vector.push_back(log_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessThresholding(std::string img_name, std::string output_name, int threshold, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> log_img = img->ProcessThresholding(output_name, threshold, progress_bar);
-    Insert(output_name, log_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> log_img = img->ProcessThresholding(img->GetImageName() + output_name, threshold, progress_bar);
+        out_img_vector.push_back(log_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessMedianFilter(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> median_img = img->ProcessMedianFilter(output_name, filter_size, progress_bar);
-    Insert(output_name, median_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> median_img = img->ProcessMedianFilter(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(median_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessErosion(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> erosion_img = img->ProcessErosion(output_name, filter_size, progress_bar);
-    Insert(output_name, erosion_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> erosion_img = img->ProcessErosion(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(erosion_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessDilatation(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> dilatation_img = img->ProcessDilatation(output_name, filter_size, progress_bar);
-    Insert(output_name, dilatation_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> dilatation_img = img->ProcessDilatation(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(dilatation_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessErosionDilatation(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> erosion_dilatation_img = img->ProcessErosionDilatation(output_name, filter_size, progress_bar);
-    Insert(output_name, erosion_dilatation_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> erosion_dilatation_img = img->ProcessErosionDilatation(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(erosion_dilatation_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessUnsharpMask(std::string img_name, std::string output_name, double alpha, bool save_mask, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> sharp_img = img->ProcessUnsharpMask(output_name, alpha, save_mask, filter_size, progress_bar);
-    Insert(output_name, sharp_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> sharp_img = img->ProcessUnsharpMask(img->GetImageName() + output_name, alpha, save_mask, filter_size, progress_bar);
+        out_img_vector.push_back(sharp_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessLMR(std::string img_name, std::string output_name, int filter_size, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> sharp_img = img->ProcessLMR(output_name, filter_size, progress_bar);
-    Insert(output_name, sharp_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> sharp_img = img->ProcessLMR(img->GetImageName() + output_name, filter_size, progress_bar);
+        out_img_vector.push_back(sharp_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessCanny(std::string img_name, std::string output_name, bool save_tmp_imgs, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> canny_img = img->ProcessCanny(output_name, save_tmp_imgs, progress_bar);
-    Insert(output_name, canny_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> canny_img = img->ProcessCanny(img->GetImageName() + output_name, save_tmp_imgs, progress_bar);
+        out_img_vector.push_back(canny_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessOtsuSegmentation(std::string img_name, std::string output_name){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> otsu_img = img->ProcessOtsuSegmentation(output_name);
-    Insert(output_name, otsu_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> otsu_img = img->ProcessOtsuSegmentation(output_name);
+        out_img_vector.push_back(otsu_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessKMeans(std::string img_name, std::string output_name, int k, KMeans::K_MEANS_DISTANCE distance_method){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> k_img = img->ProcessKMeans(output_name, k, distance_method);
-    Insert(output_name, k_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> k_img = img->ProcessKMeans(img->GetImageName() + output_name, k, distance_method);
+        out_img_vector.push_back(k_img);
+    }
+    Insert(output_name, out_img_vector);
 }
 
 void ImageBundle::ProcessNegative(std::string img_name, std::string output_name, QProgressBar *progress_bar){
-    std::shared_ptr<ImageHolder> img = FindImage(img_name);
-    std::shared_ptr<ImageHolder> neg_img = img->ProcessNegative(output_name, progress_bar);
-    Insert(output_name, neg_img);
+    std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
+    std::vector<std::shared_ptr<ImageHolder>> out_img_vector;
+    for(auto img : img_vector){
+        std::shared_ptr<ImageHolder> neg_img = img->ProcessNegative(img->GetImageName() + output_name, progress_bar);
+        out_img_vector.push_back(neg_img);
+    }
+    Insert(output_name, out_img_vector);
 }
