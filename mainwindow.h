3648605980205f5f5e84aa7445ebd937d6cb11b1
Unknown <pierre-nicolas.piquin@mines-paristech.fr>
2018-07-07 14:23:12 +0200

    Refactor a bit mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -32,49 +32,27 @@ public:
 
 private slots:
     void on_pushButtonLoadImage_clicked();
-
     void on_pushButtonEdgeDetect_clicked();
-
     void on_histogram_tab_launch_normalization_clicked();
-
     void on_intensity_tab_process_law_power_clicked();
-
     void on_intensity_tab_process_log_law_clicked();
-
     void on_gaussian_blur_push_button_clicked();
-
     void on_intensity_tab_process_thresholding_clicked();
-
     void on_median_filter_push_button_clicked();
-
     void on_erosion_push_button_clicked();
-
     void on_dilatation_push_button_clicked();
-
     void on_erosion_dilatation_push_button_clicked();
-
     void on_sharp_process_clicked();
-
     void on_change_working_dir_button_clicked();
-
     void on_save_push_button_clicked();
-
     void on_current_image_currentTextChanged(const QString &arg1);
-
     void on_lmr_filter_push_button_clicked();
-
     void on_canny_push_button_clicked();
-
     void on_otsu_push_button_clicked();
-
     void on_k_means_push_button_clicked();
-
     void on_negative_push_button_clicked();
-
     void on_load_folder_img_button_clicked();
-
     void on_save_folder_push_button_clicked();
-
     void ui_auto_update();
 
 private:
