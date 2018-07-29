ef6724817dd5b27ed4c63b9064bb76e2586e13f8
Unknown <pierre-nicolas.piquin@mines-paristech.fr>
2018-07-29 19:23:48 +0200

    Reset progress logger on diff

--- ImageClasses/imagebundle.cpp
+++ ImageClasses/imagebundle.cpp
@@ -525,6 +525,8 @@ void ImageBundle::ProcessDifference(std::string img_name, std::string output_suf
     std::vector<std::shared_ptr<ImageHolder>> img_vector = FindImageVector(img_name);
     std::vector<std::shared_ptr<ImageHolder>> out_vect;
 
+    progress_logger->ResetProgressLogger();
+
     DifferenceProcessor dp(step);
     dp.ProcessDifference(img_vector, &out_vect, output_suffix, progress_logger);
 
